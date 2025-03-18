090565b517d24970f26460a52fe1ffe4952fd8c3
SamueldeJesusdeOliveira <samuksjj@gmail.com>
2025-03-18 17:28:54 -0300

    lambda

--- ComparationTSystem/Program.cs
+++ ComparationTSystem/Program.cs
@@ -10,18 +10,17 @@ namespace ComparationTSystem {
             list.Add(new Product("Notebook", 1200.00));
             list.Add(new Product("Tablet", 450.00));
 
-            Comparison<Product> comp = CompareProduts;
+            Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
 
             list.Sort(comp);
 
+            //ou
+
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
             foreach (Product p in list)
             {
                 Console.WriteLine(p);
             }
-        }
-        static int CompareProduts(Product p1, Product p2)
-        {
-            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
         }
     }
 }
